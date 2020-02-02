394ba4a95ff83d89f83cda42c9248d5c2c7e2914
ReconChief <32560933+ReconChief@users.noreply.github.com>
2020-02-02 09:29:55 -0500

    New Voice Lines and Soundtracks

---
+++ Global Game Jam 2020/Assets/Scripts/WinVoiceOvers.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinVoiceOvers : MonoBehaviour
+{
+    private int dialogueLine = 0;
+
+    public GameObject[] dialogues = new GameObject[8];
+
+    public AudioSource[] voiceLines = new AudioSource[6];
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
